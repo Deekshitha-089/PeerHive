3b11c5d13ba8486a14d66674fbab03df3a88b2b2
Deekshitha Puppala <deekshithapuppala5@gmail.com>
2026-02-24 06:51:13 +0530

    Converted remaining files to correct places

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,10 @@ import react from "@vitejs/plugin-react"
 import path from "path"
 
 export default defineConfig({
-  root: "client",   // 👈 THIS IS THE FIX
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client/src"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
 })
